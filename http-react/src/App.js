7df9b5f96834a0f21652112c99d11f1d92abc793
Felipe <machadofelipe2016@outlook.com>
2022-06-28 15:19:35 -0300

    c3-adding and commented someone part of code

--- http-react/src/App.js
+++ http-react/src/App.js
@@ -1,6 +1,7 @@
 
 import React, {useState, useEffect} from 'react'
 import './App.css';
+import { ManageFecthing } from './hoock/custon';
 
 // remind localhost turned api
 const url = "http://localhost:3000/products"
@@ -9,17 +10,20 @@ function App() {
   const [products, setProducts] = useState([])
   const [name, setName ] = useState('')
   const [price, setPrice] = useState('')
+  
+  // now this structure manage hoock personalities!!
+  const { data:items ,HttpConfig, loading} = ManageFecthing(url)
 
  // 1-get data to pratice this method!!!
-  useEffect(() => {
-    const AddingFethcing = async () => {
-      const res = await fetch(url)
-      const data = await res.json()
-      setProducts(data)
-    }
-   AddingFethcing()
-  },[])
-  console.log('testing here', products)
+  //useEffect(() => {
+    //const AddingFethcing = async () => {
+     // const res = await fetch(url)
+      //const data = await res.json()
+      //setProducts(data)
+   // }
+   //AddingFethcing()
+  //},[])
+  //console.log('testing here', products)
  
 
   //2-create function to get all products
@@ -31,15 +35,21 @@ function App() {
    }
    console.log(product)
 
-    const res = await fetch(url, {
-    method:'POST',
-    headers:{
-      "Content-Type":'application/json'
-    },
-    body: JSON.stringify(product),
-    }) 
+    //const res = await fetch(url, {
+   // method:'POST',
+   // headers:{
+      //"Content-Type":'application/json'
+    //},
+   // body: JSON.stringify(product),
+    //}) 
+    
+    //3-create function to handling loaded dynamic
+    //const AddingProduct = await res.json()
+    //setProducts((prevProduct) => [...prevProduct, AddingProduct] )
    // const data = await res.json()
     //setProducts(data)
+   
+    HttpConfig(product, "POST")
     setName('')
     setPrice('')
   }
@@ -49,12 +59,14 @@ function App() {
     <div className="App">
       <h1>adding more testing and practice more and more always</h1>
         
-        <ul> {products.map((product) => 
+         {loading && <p>Loading date ....!</p>}
+          
+          {!loading && ( <ul>  { items && items.map((product) => 
           (<li key={product.id}>
             {product.name} 
            -R$:{product.price} 
           </li>) )} 
-        </ul>
+        </ul>)}
 
 
       <form onSubmit={HandProd}> 
