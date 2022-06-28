66f3bccac47661092fb96b81462f8c364b5e48c4
Felipe <machadofelipe2016@outlook.com>
2022-06-28 15:23:41 -0300

    c5-rearrange all structure code

--- http-react/src/App.js
+++ http-react/src/App.js
@@ -7,26 +7,15 @@ import { ManageFecthing } from './hoock/custon';
 const url = "http://localhost:3000/products"
 
 function App() {
-  const [products, setProducts] = useState([])
+  //const [products, setProducts] = useState([])
   const [name, setName ] = useState('')
   const [price, setPrice] = useState('')
   
   // now this structure manage hoock personalities!!
   const { data:items ,HttpConfig, loading} = ManageFecthing(url)
-
- // 1-get data to pratice this method!!!
-  //useEffect(() => {
-    //const AddingFethcing = async () => {
-     // const res = await fetch(url)
-      //const data = await res.json()
-      //setProducts(data)
-   // }
-   //AddingFethcing()
-  //},[])
-  //console.log('testing here', products)
  
 
-  //2-create function to get all products
+  //2-create function to get all products and adding httpconfig of hoock personalities
   const HandProd =  async (e) => {
    e.preventDefault()
    const product = {
@@ -34,21 +23,6 @@ function App() {
      price
    }
    console.log(product)
-
-    //const res = await fetch(url, {
-   // method:'POST',
-   // headers:{
-      //"Content-Type":'application/json'
-    //},
-   // body: JSON.stringify(product),
-    //}) 
-    
-    //3-create function to handling loaded dynamic
-    //const AddingProduct = await res.json()
-    //setProducts((prevProduct) => [...prevProduct, AddingProduct] )
-   // const data = await res.json()
-    //setProducts(data)
-   
     HttpConfig(product, "POST")
     setName('')
     setPrice('')
